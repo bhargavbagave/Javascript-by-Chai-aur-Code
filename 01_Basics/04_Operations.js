44f14791d76f3665f60866e40b2c480e52d146c7
Bhargav Bagave <bhargavbagave@gmail.com>
2026-09-11 15:01:34 +0000

    learned Operator

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,18 @@
+let num = 123
+let negNum = -num
+console.log(num)
+console.log(negNum)
+
+// All same operator as any other programing language
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+let gameCounter = 100
+console.log(gameCounter++)
+console.log(++gameCounter)
